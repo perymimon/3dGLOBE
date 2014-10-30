acc006b8f8719142960bb790c494cc58a276ea37
perymimon <pery.mimon@gmail.com>
2014-10-30 23:26:15 +0200

    add walk location location
    and little popup for next location

---
+++ 1click install server.cmd
@@ -0,0 +1,6 @@
+cd install
+node-v0.10.33-x64.msi
+cd..
+npm install http-server -g
+npm install bower -g
+bower install
--- install server.cmd
+++
@@ -1,4 +0,0 @@
-cd install
-node-v0.10.33-x64.msi
-cd..
-npm install http-server -g
